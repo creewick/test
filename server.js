a471aba827e21970b772db2f370f3e1963e6e573
Юхатский Илья Владиславович <ilia.y@skbkontur.ru>
2019-01-30 14:21:58 +0500

    вжух

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+function getSettings() {
+    let arg = process.argv;
+    return {
+        groupKey: arg[1],
+        serviceKey: arg[2],
+        confirmCode: argv[3],
+        replyString: argv[4],
+        databaseToken: argv[5],
+        databaseName: argv[6]
+    };
+}
+
+function start() {
+    const app = express();
+
+    app.get('/', (_, res) => {
+        res.send('API is running');
+    });
+    
+    app.listen(1337);
+}
+
+start();
